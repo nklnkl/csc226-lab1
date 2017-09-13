9ed02e38f6a86681363c86db76ab038351524013
Niko Lagman <Lagman.Niko@gmail.com>
2017-09-13 16:36:23 -0400

    Add categoryString and unitString to item class

--- objects/item.php
+++ objects/item.php
@@ -6,9 +6,14 @@
 		// Should be of type float.
 		private $price;
 
+		// 0: toy, 1: dry food, 2: wet food
+		private $category;
+
 		/*
 		 * The type of quantity this item has.
-		 * pounds
+		 * 0: unit, no label
+		 * 1: pound
+		 * 2: can
 		 */
 		private $unit;
 
@@ -21,6 +26,9 @@
 		public function getPrice () {
 			return $this->price;
 		}
+		public function getCategory () {
+			return $this->category;
+		}
 		public function getUnit () {
 			return $this->unit;
 		}
@@ -29,16 +37,37 @@
 		}
 
 		public function setName ($name) {
-			$this->name = name;
+			$this->name = $name;
 		}
 		public function setPrice ($price) {
-			$this->price = price;
+			$this->price = $price;
+		}
+		public function setCategory ($category) {
+			$this->category = $category;
 		}
 		public function setUnit ($unit) {
-			$this->unit = unit;
+			$this->unit = $unit;
 		}
 		public function setQuantity ($quantity) {
-			$this->quantity = quantity;
+			$this->quantity = $quantity;
+		}
+
+		public function getCategoryString () {
+			switch ($this->category) {
+				case 0: return "toy";
+				case 1: return "dry food";
+				case 2: return "wet food";
+				default: return;
+			}
+		}
+
+		public function getUnitString () {
+			switch ($this->category) {
+				case 0: return "unit";
+				case 1: return "pound";
+				case 2: return "can";
+				default: return;
+			}
 		}
 	}
 ?>
